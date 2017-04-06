0c35c75288745085cf9aade90a763f2ae25bec11
ntian <1179953947@qq.com>
2017-04-06 15:37:06 +0800

    初始化项目

---
+++ spade/spade/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  spade
+//
+//  Created by ntian on 2017/4/6.
+//  Copyright © 2017年 ntian. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ spade/spade/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spade
+//
+//  Created by ntian on 2017/4/6.
+//  Copyright © 2017年 ntian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
